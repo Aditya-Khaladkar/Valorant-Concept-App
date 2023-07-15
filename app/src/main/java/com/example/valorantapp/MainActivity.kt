8224dff25f4cb518e11b7eb35146c4b2e080a105
Aditya-Khaladkar <adityakhaladkar98@gmail.com>
2023-07-15 18:50:56 +0530

    project setup

--- app/src/main/java/com/example/valorantapp/MainActivity.kt
+++ app/src/main/java/com/example/valorantapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.valorantapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed(Runnable {
+
+        },2000)
     }
 }
---
+++ app/src/main/java/com/example/valorantapp/auth/WelcomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.valorantapp.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.valorantapp.R
+
+class WelcomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+    }
+}
