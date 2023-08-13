86924a6e7cc413885798fd633fd93d7cc94ac3ab
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2023-08-13 16:37:35 +0530

    Add: called maps api

--- app/src/main/java/com/example/valorantapp/adapter/map/MapAdapter.kt
+++ app/src/main/java/com/example/valorantapp/adapter/map/MapAdapter.kt
@@ -1,4 +1,41 @@
 package com.example.valorantapp.adapter.map
 
-class MapAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.valorantapp.R
+import com.example.valorantapp.adapter.weapon.WeaponAdapter
+import com.example.valorantapp.model.map.MapData
+import com.example.valorantapp.model.weapon.WeaponData
+
+class MapAdapter(private val list: List<MapData>): RecyclerView.Adapter<MapAdapter.ViewHolder>(){
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val displayName: TextView = itemView.findViewById(R.id.txtDisplayName)
+        val displayIcon: ImageView = itemView.findViewById(R.id.imageView)
+        val displayCord: TextView = itemView.findViewById(R.id.txtDisplaycoordinates)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.map_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val map = list[position]
+        holder.displayName.text = map.displayName
+        holder.displayCord.text = map.displayCoordinate
+        Glide.with(holder.displayIcon.context)
+            .load(map.displayIcon)
+            .into(holder.displayIcon)
+
+    }
 }
