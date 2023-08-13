798dd012360deb7b301434f170406d2f9ff8b4b6
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2023-08-13 12:13:14 +0530

    starting creation of new activity

--- app/src/main/java/com/example/valorantapp/view/ui/main/PlaceholderFragment.kt
+++ app/src/main/java/com/example/valorantapp/view/ui/main/PlaceholderFragment.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.valorantapp.view.R
+
 import com.example.valorantapp.databinding.FragmentWeaponsMapsBinding
 
 /**
@@ -16,19 +16,13 @@ import com.example.valorantapp.databinding.FragmentWeaponsMapsBinding
  */
 class PlaceholderFragment : Fragment() {
 
-    private lateinit var pageViewModel: PageViewModel
     private var _binding: FragmentWeaponsMapsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        pageViewModel = ViewModelProvider(this).get(PageViewModel::class.java).apply {
-            setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
-        }
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,10 +32,7 @@ class PlaceholderFragment : Fragment() {
         _binding = FragmentWeaponsMapsBinding.inflate(inflater, container, false)
         val root = binding.root
 
-        val textView: TextView = binding.sectionLabel
-        pageViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
         return root
     }
 
--- app/src/main/java/com/example/valorantapp/view/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/valorantapp/view/ui/main/SectionsPagerAdapter.kt
@@ -4,12 +4,7 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.example.valorantapp.view.R
 
-private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
-)
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
@@ -24,9 +19,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
         return PlaceholderFragment.newInstance(position + 1)
     }
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
-    }
 
     override fun getCount(): Int {
         // Show 2 total pages.
